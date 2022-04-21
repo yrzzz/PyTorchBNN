663478de4688d5efe06b878bf28d82a64de91c81
Ruizhi Yuan <94030641+yrzzz@users.noreply.github.com>
2022-04-21 15:49:24 -0400

    Delete base_bayesian_module.py

--- BayesianNeuralNetwork/modules/base_bayesian_module.py
+++
@@ -1,8 +0,0 @@
-from torch import nn
-class BayesianModule(nn.Module):
-    """
-    Create a base class for Bayesian Neural Network, so that other functions can distinguish the Bayesian layer
-    """
-
-    def init(self):
-        super().__init__()
